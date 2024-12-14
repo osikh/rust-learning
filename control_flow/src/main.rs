646593b70700fb8de5e5736ca9f5bb5628596171
Harpreet Singh <idlejatt@gmail.com>
2024-12-14 12:49:51 +0530

    control flow

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
-    println!("Hello, world!");
+
+    let mut i = 0;
+    loop {
+        println!("Loop... i: {}", i);
+        i = i+1;
+
+        if i == 10 {
+            break i = 100;
+        }
+    }
+
+    println!("index value: {}", i);
+
+    let x = 5;
+
+    if x > 5 {
+        println!("x is greater than 5");
+    } else {
+        println!("x is less than or equal to 5");
+    }
+
+    // ---- error
+    // if x {
+    //     println!("x is bool");
+    // }
+
+    if x != 1 {
+        println!("x is not equal to 1");
+    }
+
+    if x % 2 == 0 {
+        println!("x is divisible by 2");
+    }
+
+    if x % 5 == 0 {
+        println!("x is divisible by 5");
+    }
+
+    let x = if x > 5 { 6 } else { 4 };
+
+    println!("now x value is changed to: {}", x);
 }
